1555e16b43bd8eb4d3fb1201fcb1aa831bd4e9c0
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2019-02-28 16:08:46 +0700

    change to vue-point-table

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import PointTable from 'vue-table-blegoh1'
+import PointTable from 'vue-point-table'
 import './../node_modules/bulma/css/bulma.css'
 
 Vue.config.productionTip = false
